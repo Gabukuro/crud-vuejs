682ed98aa0ab254231b59b242a1f224a5bbd5960
Daniel Schmitz <danieljfa@gmail.com>
2016-07-07 22:49:29 -0300

    App com bulma

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <h1>{{ msg }}</h1>
   </div>
 </template>
@@ -8,18 +8,8 @@
 export default {
   data () {
     return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello Vue!'
+      msg: 'Vue.js Crud'
     }
   }
 }
 </script>
-
-<style>
-body {
-  font-family: Helvetica, sans-serif;
-}
-</style>
